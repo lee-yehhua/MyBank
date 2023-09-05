2ab1a45446bb345b32862b31838d1a33d11ca169
李晔华 <48814823+lee-yehhua@users.noreply.github.com>
2023-09-05 19:33:15 +0800

    Initial commit

---
+++ db/sqlc/store.go
@@ -0,0 +1,114 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// Store 提供所有函数来执行db queries和transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		New(db),
+		db,
+	}
+}
+
+// 输入context和回调函数，用创建的Queries调用回调函数，最后根据函数返回的error commit或者rollback事务
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil) //&sql.TxOptions设置隔离级别,read-committed
+	if err != nil {
+		return err
+	}
+	//事务中执行的queries
+	q := New(tx)
+	//调用输入的回调函数
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer Transfer `json:"transfer"`
+
+	FromAccount Account `json:"from_account"`
+	ToAccount   Account `json:"to_account"`
+	FromEntry   Entry   `json:"from_entry"`
+	ToEntry     Entry   `json:"to_entry"`
+}
+
+// TransferTx 创建转账记录、 添加账户条目、更新账户余额，在一个数据库事务中
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			arg.FromAccountID,
+			arg.ToAccountID,
+			arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			arg.FromAccountID,
+			-arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			arg.ToAccountID,
+			arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// update accounts' balance
+		//按ID顺序操作，防止死锁
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = addMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+		} else {
+			result.ToAccount, result.FromAccount, err = addMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+		}
+		return err
+	})
+	return result, err
+}
+
+func addMoney(ctx context.Context,
+	q *Queries,
+	accountID1 int64,
+	amount1 int64,
+	accountID2 int64,
+	amount2 int64) (account1 Account, account2 Account, err error) {
+	account1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		amount1,
+		accountID1,
+	})
+	if err != nil {
+		return
+	}
+	account2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		amount2,
+		accountID2,
+	})
+	return
+}
