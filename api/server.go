b6dd29d91d81f9c9dd9c9b4f2f58644644b77d49
李晔华 <48814823+lee-yehhua@users.noreply.github.com>
2023-09-07 10:59:21 +0800

    11. implement restful http api using gin

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/lee-yehhua/simplebank/db/sqlc"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+// Start 根据输入的地址运行Http server，并开始监听API请求
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
